6e9a40a0a22dfa54d7ea0f4ae5dfcdd9266741da
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-10-02 22:53:33 +0100

    set up files

---
+++ specs/classes_homework__library__specs.rb
@@ -0,0 +1,20 @@
+require ("minitest/autorun")
+require ("minitest/rg")
+require_relative("../classes_homework__library.rb")
+
+class TestLibrary < MiniTest::Test
+
+  def setup
+
+#Create a class for a Library that has an array of books. Each book should be a hash with a title, which is a string, and rental details, which is another hash with a student name and due date.
+#
+#   {
+#     title: "lord_of_the_rings",
+#     rental_details: {
+#      student_name: "Jeff",
+#      date: "01/12/16"
+#     }
+#   }
+  end
+
+end #class end
